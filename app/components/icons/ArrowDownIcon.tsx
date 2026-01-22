bb251588b48ebbaeebf036eb6270873738633ac5
DevKirito <manlangitjasonrey@gmail.com>
2026-01-23 03:25:54 +0800

    update

--- app/components/icons/ArrowDownIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ArrowDown from './arrow-down.svg';
-
-const ArrowDownIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
-  <img src={ArrowDown} alt="Arrow Down" {...props} />
-);
-
-export default ArrowDownIcon;
